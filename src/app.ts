2aabf049c35c229cafb6e75d216a1409d843ce4b
Mo Amir <130815672+mo-amir-code@users.noreply.github.com>
2024-03-20 16:23:44 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ import cors from "cors"
 const app: Express = express();
 
 const corsOptions = {
-    origin: process.env.DEVELOPMENT === "development"? 'http://localhost:3000' : "https://my-memik.vercel.app", // Replace with your frontend URL
+    origin: process.env.CLIENT_ORIGIN,
     credentials: true, // Allow cookies
 };
 
