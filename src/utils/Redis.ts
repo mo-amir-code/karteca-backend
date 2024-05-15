051dee4af48e1d428654a4430d2f6ad15afbb640
Mo Amir <mo.amir.code@gmail.com>
2024-05-15 22:09:02 +0530

    redis connection error handling added

--- src/utils/Redis.ts
+++ src/utils/Redis.ts
@@ -2,11 +2,14 @@ import {Redis} from "ioredis";
 
 
 let redisInstance;
-
-if (process.env.REDIS_URI) {
-    redisInstance = new Redis(process.env.REDIS_URI);
-} else {
-    redisInstance = new Redis(6379);
+try {
+    if (process.env.REDIS_URI) {
+        redisInstance = new Redis(process.env.REDIS_URI);
+    } else {
+        redisInstance = new Redis(6379);
+    }
+} catch (error) {
+    console.error("Redis Error ===> ", error)
 }
-
+    
 export const redis = redisInstance;
